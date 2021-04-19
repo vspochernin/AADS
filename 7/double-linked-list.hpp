c196cf7562343c3d81f41f8012ca90354358834c
vladislav-pochernin <vspochernin@gmail.com>
2021-04-19 13:30:22 +0300

    Delete extra things

--- 7/double-linked-list.hpp
+++ 7/double-linked-list.hpp
@@ -8,21 +8,13 @@
 class DoubleLinkedList
 {
 public:
-  DoubleLinkedList(); // TODO:перенести: count_(0), head_(nullptr), tail_(nullptr) {}   // Конструктор "по умолчанию" - создание пустого списка.
+  DoubleLinkedList(); // Конструктор "по умолчанию" - создание пустого списка.
 
-  virtual ~DoubleLinkedList(); // Деструктор списка
+  virtual ~DoubleLinkedList(); // Деструктор списка.
 
-  //TODO:возможно удалитьfriend std::ostream& operator<< (std::ostream& out, const DoubleLinkedList& list); // Перегрузка оператора <<.
-
-  size_t count()const; // TODO:Перенести{ return count_; }   // Количество элементов списка.
-  //TODO:возможно удалитьint headItem() const; // Константный доступ к информации головного узла списка
-  //TODO:возможно удалитьint& headItem(); // Доступ к информации головного узла списка
-  //TODO:возможно удалитьint tailItem() const; // Константный доступ к информации хвостового узла списка
-  //TODO:возможно удалитьint& tailItem(); // Доступ к информации хвостового узла списка
+  size_t count() const; // Количество элементов списка.
   void insertItem(const std::string& key); // Добавление узла с заданным ключом (или увеличение счетчика частотности).
-  bool deleteItem(const std::string& key); // Удаление узла с заданным ключом.  //TODO: удалил bool all = false
-  //TODO:возможно удалитьbool searchItem(const std::string& key); // Поиск записи с заданным ключом.
-  //TODO:возможно удалитьvoid outAll(); // Вывод элементов списка в текстовом виде в стандартный выходной поток
+  bool deleteItem(const std::string& key); // Удаление узла с заданным ключом.
   void clear();  // Очищение списка
 
 private:
@@ -48,10 +40,6 @@ private:
   Node* head_;        // Указатель на первый элемент списка.
   Node* tail_;        // Указатель на последний элемент списка.
 
-  //TODO:возможно удалитьNode* head() const; //TODO: перенести { return head_; } // Доступ к головному узлу списка.
-  //TODO:возможно удалитьNode* tail() const; //TODO: перенести { return tail_; } // Доступ к хвостовому узлу списка.
-  //TODO:возможно удалитьvoid insertTail(Node* x); // Всавить сформированный узел в хвост списка.
-  //TODO:возможно удалитьvoid insertHead(Node* x); // Вставить сформированный узел в начало списка. //TODO: разобраться (int item);
   void insertNode(Node* x); // Вставить сформированный узел в список.
   void deleteNode(Node* x); // Удаление заданного узла.
   Node* searchNode(std::string key); // Поиск узла (адрес) с заданным значением ключа.
